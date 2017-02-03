d39ab17bcb1f4224639a85ec65cf9f4c7849e3aa
michaeldeongreen <michael.green@architectinginnovation.com>
2017-02-03 07:54:31 -0600

    *After fixing a few issues, the application is running locally

---
+++ src/ContosoAds.Common/ContosoAdsContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoAds.Common
+{
+    public class ContosoAdsContext : DbContext
+    {
+        public ContosoAdsContext() : base("name=ContosoAdsContext")
+        {
+        }
+        public ContosoAdsContext(string connString)
+            : base(connString)
+        {
+        }
+        public DbSet<Ad> Ads { get; set; }
+    }
+}
